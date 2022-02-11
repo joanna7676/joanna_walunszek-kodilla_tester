17200289baae12a97521572d86c6965823b3236f
Joanna Walunszek <jopi76@gazeta.pl>
2022-02-11 17:22:03 +0100

    initial version

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,8 +1,6 @@
 public class HelloWorld {                           // [1]
     public static void main(String[] args) {         // [2]
-        System.out.println("Hello World!");// [3]
-        System.out.println("Joanna");
-        System.out.println("46");
-        System.out.println("W");
+        String haslo = "Hello from First Class";
+        System.out.println(haslo);
     }
 }
---
+++ src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myName = "Joanna";
+        String myTown = "Stargard";
+        int myAge = 46;
+        double height = 1.65;
+        char group = 'A';
+        boolean GoodCook = true;
+        System.out.println(myName);
+        System.out.println(myTown);
+        System.out.println(myAge);
+        System.out.println(height);
+        System.out.println(group);
+        System.out.println(GoodCook);
+    }
+}
