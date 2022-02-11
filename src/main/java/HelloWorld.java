d9b77e6d9436f3d89f197f836b9d105af86ef031
Joanna Walunszek <jopi76@gazeta.pl>
2022-02-11 14:48:47 +0100

    initial commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {                           // [1]
+    public static void main(String[] args) {         // [2]
+        System.out.println("Hello World!");// [3]
+        System.out.println("Joanna");
+        System.out.println("46");
+        System.out.println("W");
+    }
+}
